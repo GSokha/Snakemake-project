f7b05e0283a6dd9cfcc95866dfb52f104987a53e
GSokha <89578142+GSokha@users.noreply.github.com>
2023-05-04 00:23:09 +0400

    Update counts.py

--- tools/counts.py
+++ tools/counts.py
@@ -11,11 +11,9 @@ for files in data:
     elif files.startswith('KAPA'):
         kapa_files.append(files)
 
-# Join all Collibri file names with a space separator
 collibri_files_str = " ".join(collibri_files)
 kapa_files_str = " ".join(kapa_files)
 
-# Run featureCounts on all Collibri files simultaneously
 subprocess.run("featureCounts -p -t exon -g gene_id -a /home/quantori/snake/data/play_data_ref_annot/chr19_20Mb.gtf -o counts_collibri.txt {collibri_files_str} -s 1 ".format(collibri_files_str=collibri_files_str),shell=True)
 
 subprocess.run("featureCounts -p -t exon -g gene_id -a /home/quantori/snake/data/play_data_ref_annot/chr19_20Mb.gtf -o counts_kapa.txt {kapa_files_str} -s 2 ".format(kapa_files_str=kapa_files_str),shell=True)
